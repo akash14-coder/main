0b3011614e8ffeca3aae3454f8e7172d9934e6d4
akash14-coder <akashjadav521@gmail.com>
2025-04-14 19:16:06 +0530

    Update chains.py

--- chains.py
+++ chains.py
@@ -1,4 +1,5 @@
 import os
+from chains import Chain  # If `Chain` is a class inside `chains.py`
 from langchain_groq import ChatGroq
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import JsonOutputParser
